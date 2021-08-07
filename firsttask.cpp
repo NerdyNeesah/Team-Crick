1481a97ce72d4716da52164cbe858637abe08c27
Sanniya01 <88289451+Sanniya01@users.noreply.github.com>
2021-08-07 11:11:26 +0530

    C++ program by Sanniya

---
+++ firsttask.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+ {
+    string s1, s2;
+    s1 = "@Sanniya01";
+    s2 = "@Sanniya01";
+
+    int i = 0, count = 0;
+    while (s1[i] != '\0')
+    {
+        if (s1[i] != s2[i])
+            count++;
+        i++;
+    }
+
+    cout<< "Sanniya Middha"<<","<<"sanniyamiddha12@gmail.com"<<","<<s1<<","<<"Genomics"<<","<<s2<<","<<count<<endl;
+    
+    return 0;
+}
