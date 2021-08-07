4e4ac65181d605245414a39d53d3d4ded13294e7
Fredrick-kakembo <Kakembofredrick@gmail.com>
2021-08-07 20:32:04 +0300

    Edit the bash script

--- bashscript.sh
+++ bashscript.sh
@@ -23,21 +23,6 @@ do
 	fi
 done 
 
-if test -f "$FILE"; then
-    rm $FILE             #remove if there is any executable file is present
-fi
-
-PROG_NAME=firsttask.cpp       #name of my c++ program
-echo $PROG_NAME           #print name of pragram name
-
-output=$(g++ $PROG_NAME)  #compile program
-echo $output              #print if there is any error
-
-
-if test -f "$FILE"; then  #check if we have our c++ program compiled succssfully
-                          #if yes, then execute it and clean up the executable
-    RESULT=$(./$FILE params) #save the output from our program to RESULT
-
     echo $RESULT >> Team_crick.csv #append thes result in the csv file
     rm $FILE 
 fi
